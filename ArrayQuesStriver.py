8d25b9cbfb6bc100e9e6ec1438fe149e1dc626fb
shantanu-dahitule <shantanu05.dahitule@gmail.com>
2024-10-21 22:48:33 +0530

    Array Questions

--- ArrayQuesStriver.py
+++ ArrayQuesStriver.py
@@ -3,8 +3,8 @@
                             Arrays and Questions
 """
 
-arr = [1, 3, 87, 85, 39, 14, 78, 8]
-n = len(arr)
+# arr = [1, 3, 87, 0,0,0,0,85, 39, 14, 78, 8]
+# n = len(arr)
 ## Largest element of array
 # # 1st approach
 # print("Max element: ",max(arr))
@@ -63,3 +63,119 @@ n = len(arr)
 #         j+=1
 # print(arr)    
 # print(i+1)
+
+# ## Left Rotate Array
+# n=int(input("How many palces wants to rotate: "))
+# for j in range(n):
+#     f2l = arr[0]
+#     for i in range(1,len(arr)):
+#         arr[i-1] = arr[i]
+#     arr[len(arr)-1] = f2l
+# print(arr)
+
+## Put zeroes at end
+# arr = [1,2,0,3,0,0,4]
+# i=0
+# j=0
+# while(j!=len(arr)-1):
+#     if arr[i]!=0:
+#         print("i: ",i)
+#         i+=1
+#     if arr[j]==0 and j>i:
+#         print("j: ",j)
+#     j += 1
+#     arr[i],arr[j] = arr[j],arr[i]
+    
+# print(arr)    
+    
+## Union of Array
+# # Brute Forece Approach
+# arr1 = [1,2,3,4,5,6]
+# arr2 = [3,4,5,6,7,9]
+# union = []
+# # freq = {}
+# # for num in arr1:
+# #     freq[num] = freq.get(num,0)+1
+    
+# # for num in arr2:
+# #     freq[num] = freq.get(num,0)+1
+
+# # for num in freq:
+# #     union.append(num)
+# # print(union)
+
+# ### Optimal Approach 2 pointer method
+# i=0
+# j=0
+# while(i<len(arr1) and j<len(arr2)):
+#     if arr1[i]<arr2[j]:
+#         union.append(arr1[i])
+#         i+=1
+#     if arr1[i]>=arr2[j]:
+#         union.append(arr2[j])
+#         i+=1
+#         j+=1
+# while(i<len(arr1)):
+#     union.append(arr1[i])
+#     i+=1
+# while(j<len(arr2)):
+#     union.append(arr2[j])
+#     j+=1
+# print(union)    
+
+## Find missing number in sequence
+# arr = [1,2,3,5,6]
+# n = len(arr)+1
+# esum = int(n*(n+1)/2)
+# tsum=0
+# for i in range(len(arr)):
+#     tsum = tsum+arr[i]
+# print(esum-tsum)
+
+
+## Maximum Consecutive ones 
+# arr = [1, 0, 1, 1, 0, 1]
+# max1 = 0
+# count=0
+# for i in range(len(arr)):
+#     if arr[i]==1:
+#         count+=1
+#     if count>=max1:
+#         max1 = count
+#     if arr[i]==0:
+#         count = 0
+# print("Maximum ones together: ",max1)
+
+# ## Find the number that appears once, and the other numbers twice
+# arr = [4,1,2,1,2]
+# freq = {}
+# for i in range(len(arr)):
+#     freq[arr[i]] = freq.get(arr[i],0)+1
+# for i in freq:
+#     if freq[i]==1:
+#         print(i)
+
+# ## Approach 2
+# xor = 0
+# for i in range(len(arr)):
+#     xor = xor^arr[i]
+
+# print(xor)
+
+        
+        
+        
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
